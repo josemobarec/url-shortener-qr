7f14d627ab99591f352707174aab24b59cb9056b
José Luis Beltrán Mobarec <jose.mobarec@gmail.com>
2026-03-10 15:07:44 -0300

    add rate limiting to protect shorten endpoint

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const urlController = require("../controllers/urlController");
+const shortenLimiter = require("../middleware/rateLimiter");
 
-router.post("/shorten", urlController.shortenUrl);
+router.post("/shorten", shortenLimiter, urlController.shortenUrl);
 
 router.get("/stats/:short_code", urlController.getStats);
 
